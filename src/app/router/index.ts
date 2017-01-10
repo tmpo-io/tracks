a48ad1d0375f92f2d60a8410c74d4348bf2dd2df
Jordi Collell <jordic@gmail.com>
2017-01-10 07:21:44 +0100

    ideas

---
+++ src/app/router/index.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+/*
+A lifted Router module
+
+*/
+
+
+// => Observable of router states..
+
+@Injectable()
+export class RouterService {
+
+  constructor() {}
+
+
+
+}
+
