1631e7517488419c7a1bfe70064cd274789db019
Jordi Collell <jordic@gmail.com>
2017-01-10 06:09:23 +0100

    experimental transition on card disappear

--- src/app/pages/home.component.ts
+++ src/app/pages/home.component.ts
@@ -2,6 +2,7 @@ import {
   Component, Input, OnInit,
   ChangeDetectionStrategy
 } from '@angular/core';
+import { state, style, transition, animate, trigger } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 
@@ -13,7 +14,18 @@ declare var window: Window;
 @Component({
   selector: 'app-page-home',
   templateUrl: './home.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('in', [
+      state('*', style({
+        transform: 'scaleY(1)'
+      })),
+      state('void', style({
+        transform: 'scaleY(0)'
+      })),
+      transition('* => void', animate('0.3s ease-out')),
+    ])
+  ]
 })
 export class PageHomeComponent {
 
