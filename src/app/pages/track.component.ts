32aa362fd6e1d471f8229dcaf2ddb03b52df8bf1
jordi <jordic@gmail.com>
2017-01-11 10:51:51 +0100

    WIP log table

--- src/app/pages/track.component.ts
+++ src/app/pages/track.component.ts
@@ -1,19 +1,63 @@
 import { Component, Input } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+
 import { Track } from '../store/model';
-
 import { RouterService } from '../router';
-
+import * as actions from '../store/actions';
 
 @Component({
   selector: 'app-page-track',
-  templateUrl: './track.component.html'
+  templateUrl: './track.component.html',
 })
 export class PageTrackComponent {
-  @Input() track: Track;
 
-  constructor(public router: RouterService) { }
+  @Input() set track(track: Track) {
+    this._track = track;
+  }
 
+  get track(): Track {
+    return this._track;
+  }
 
+  private _track: Track;
+
+  constructor(
+    public router: RouterService,
+    private store: Store<any>
+    ) { }
+
+  get table(): any {
+
+    if (this._track.kind === 'time') {
+      return this.timeTable();
+    }
+    return this.counterTable();
+  }
+
+  timeTable() {
+    return this._track.logs
+      .filter(t => t.action === 'stop')
+      .map(t => ({
+        started: t.time - t.amount,
+        stopped: t.time,
+        amount: t.amount,
+        id: t.id
+      }));
+  }
+
+  counterTable() {
+    return this._track.logs
+      .filter(t => t.action === 'track');
+  }
+
+  trackFunc(ind, obj) {
+    return obj.id;
+  }
+
+  delete(id) {
+    this.router.go('');
+    this.store.dispatch(actions.trackDelete(id));
+  }
 
 }
